5de3271bd8396cee402df87f8922d54d44f7822e
TBM85 <tballesterm@gmail.com>
2021-05-10 17:10:27 -0400

    Includes a functional minute hand for the clock

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let secondsHand = document.querySelector('.seconds-hand');
+let minuteHand = document.querySelector('.minute-hand');
 
 const setDate = () => {
   const now = new Date();
@@ -6,6 +7,11 @@ const setDate = () => {
   const secondsDegrees = Math.round(((seconds / 60) * 360) + 90);
 
   secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = Math.round(((minutes / 60) * 360) + 90);
+
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
